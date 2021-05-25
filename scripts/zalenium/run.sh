3b9af79069454c3e8645c12bb930edb3c1f42b50
tflasche <tilo.flasche@qytera.de>
2021-05-25 12:03:40 +0200

    Added debuggable selenium containers

---
+++ scripts/zalenium/run.sh
@@ -0,0 +1,7 @@
+# Pull docker-selenium
+docker pull elgalu/selenium;
+
+# Pull Zalenium
+docker pull dosel/zalenium;
+
+docker run --rm -ti --name zalenium -p 4444:4444  -v /var/run/docker.sock:/var/run/docker.sock -v videos:/home/seluser/videos --privileged dosel/zalenium start;
