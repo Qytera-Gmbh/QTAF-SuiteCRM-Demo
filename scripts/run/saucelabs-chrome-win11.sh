a17e7b46f92f06b21874739ef2069535ec76706a
tflasche <tilo.flasche@qytera.de>
2022-02-02 17:27:21 +0100

    Added SauceLabs scripts

---
+++ scripts/run/saucelabs-chrome-win11.sh
@@ -0,0 +1,7 @@
+mvn exec:java -Dexec.mainClass="de.qytera.suite_crm.TestRunner" \
+  -Ddriver.name="sauce" \
+  -Ddriver.remoteUrl="https://ondemand.eu-central-1.saucelabs.com/wd/hub" \
+  -Dsauce.browserName="chrome"
+  -Ddriver.version="latest" \
+  -Ddriver.platform="Windows 11" \
+  -DpageUrl="http://suitecrm-demo.eba-xfgdbnjt.eu-central-1.elasticbeanstalk.com"
