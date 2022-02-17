f3eff9cf5a459ea1d9343433f633ff390f2b676f
tq-development <90603395+tq-development@users.noreply.github.com>
2022-02-17 20:51:35 +0100

    Create QyteraTest.java

---
+++ src/test/java/de/qytera/suite_crm/tests/QyteraTest.java
@@ -0,0 +1,20 @@
+package de.qytera.suite_crm.tests;
+
+import de.qytera.qtaf.core.config.annotations.TestFeature;
+import de.qytera.suite_crm.SuiteCRMTestContext;
+import org.testng.annotations.Test;
+
+import javax.inject.Singleton;
+
+@TestFeature(
+        name = "Calls form",
+        description = "Create call form"
+)
+@Singleton
+public class QyteraTest extends SuiteCRMTestContext {
+    @Test(testName = "QTAF-567", description = "Calls Test", dependsOnGroups = {"login"})
+    public void testCalls() {
+        //Navigate to calls page
+        qyteraPO.goToRootPage();
+    }
+}
