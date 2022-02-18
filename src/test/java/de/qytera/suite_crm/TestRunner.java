36abfd8834f32c2ac1221e198725cdb50b007b39
Maximilian Bodsch <maximilian.bodsch@qytera.de>
2022-02-18 15:14:50 +0100

    change Runner for mvn

--- src/test/java/de/qytera/suite_crm/TestRunner.java
+++ src/test/java/de/qytera/suite_crm/TestRunner.java
@@ -1,6 +1,12 @@
 package de.qytera.suite_crm;
 
 import de.qytera.qtaf.testng.QtafTestNGRunner;
+import de.qytera.qtaf.testng.event_listener.TestNGEventListener;
+import de.qytera.qtaf.testng.test_factory.TestNGFactory;
+import org.testng.TestNG;
+import org.testng.annotations.Test;
+
+import java.util.Collections;
 
 /**
  * Main runner class that runs the test classes.
@@ -8,4 +14,11 @@ import de.qytera.qtaf.testng.QtafTestNGRunner;
  */
 public class TestRunner extends QtafTestNGRunner {
 
+    @Test
+    public void run(){
+        TestNG testNG = new TestNG();
+        testNG.setListenerClasses(Collections.singletonList(TestNGEventListener.class));
+        testNG.setTestClasses(new Class[]{TestNGFactory.class});
+        testNG.run();
+    }
 }
