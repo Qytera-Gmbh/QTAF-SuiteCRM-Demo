71bbc75c22cfadb6055e1bdcc52d5a306967f05f
tflasche <tilo.flasche@qytera.de>
2023-10-09 13:22:07 +0200

    Updated LoginFormPO

--- scripts/run/test-execution-plans.sh
+++ scripts/run/test-execution-plans.sh
@@ -1,22 +1,22 @@
 mvn clean compile
 
 # Test groups
-mvn test -Dgroups=login
-mvn test -Dgroups=login,calls
-mvn test -Dgroups=login,contacts
-mvn test -Dgroups=login,meetings
-mvn test -Dgroups=login,tasks
+mvn test -Ddriver.headless=1 -Dgroups=login
+mvn test -Ddriver.headless=1 -Dgroups=login,calls
+mvn test -Ddriver.headless=1 -Dgroups=login,contacts
+mvn test -Ddriver.headless=1 -Dgroups=login,meetings
+mvn test -Ddriver.headless=1 -Dgroups=login,tasks
 
 # Test features
-mvn test -Dgroups=Logintest
-mvn test -Dgroups=LoginTest,CallsTest
-mvn test -Dgroups=LoginTest,ContactsTest
-mvn test -Dgroups=LoginTest,MeetingsTest
-mvn test -Dgroups=LoginTest,TasksTest
+mvn test -Ddriver.headless=1 -Dtest=Logintest
+mvn test -Ddriver.headless=1 -Dtest=LoginTest,CallsTest
+mvn test -Ddriver.headless=1 -Dtest=LoginTest,ContactsTest
+mvn test -Ddriver.headless=1 -Dtest=LoginTest,MeetingsTest
+mvn test -Ddriver.headless=1 -Dtest=LoginTest,TasksTest
 
 # Test scenarios
-mvn test -Dgroups=Logintest#testLogin
-mvn test -Dgroups=LoginTest,CallsTest#testCalls
-mvn test -Dgroups=LoginTest,ContactsTest#testContacts
-mvn test -Dgroups=LoginTest,MeetingsTest#testMeetings
-mvn test -Dgroups=LoginTest,TasksTest#testTasks
+mvn test -Ddriver.headless=1 -Dtest=Logintest#testLogin
+mvn test -Ddriver.headless=1 -Dtest=LoginTest,CallsTest#testCalls
+mvn test -Ddriver.headless=1 -Dtest=LoginTest,ContactsTest#testContacts
+mvn test -Ddriver.headless=1 -Dtest=LoginTest,MeetingsTest#testMeetings
+mvn test -Ddriver.headless=1 -Dtest=LoginTest,TasksTest#testTasks
--- src/test/java/de/qytera/suite_crm/page_objects/LoginFormPO.java
+++ src/test/java/de/qytera/suite_crm/page_objects/LoginFormPO.java
@@ -3,6 +3,9 @@ package de.qytera.suite_crm.page_objects;
 import de.qytera.qtaf.core.guice.annotations.Step;
 import de.qytera.qtaf.testng.context.QtafTestNGContext;
 import org.openqa.selenium.By;
+import org.openqa.selenium.InvalidArgumentException;
+
+import static com.codeborne.selenide.Selenide.$;
 
 
 /**
@@ -15,12 +18,25 @@ public class LoginFormPO extends QtafTestNGContext {
 
     @Step(name = "Fill username field", description = "Fill the username field")
     public void fillUsernameField(String username) {
-        driver.findElement(usernameFieldSelector).sendKeys(username);
+        $(usernameFieldSelector).type(username);
+        String actualValue = $(usernameFieldSelector).val();
+        assertEquals(
+                actualValue,
+                username,
+                "Expected value of username field to be '%s' and it was '%s'".formatted(username, actualValue)
+        );
+        throw new InvalidArgumentException("This is an invalid argument");
     }
 
     @Step(name = "Fill password field", description = "Fill the password field")
     public void fillPasswordField(String password) {
-        driver.findElement(passwordFieldSelector).sendKeys(password);
+        $(passwordFieldSelector).type(password);
+        String actualValue = $(passwordFieldSelector).val();
+        assertEquals(
+                actualValue,
+                password,
+                "Expected value of password field to be '%s' and it was '%s'".formatted(password, actualValue)
+        );
     }
 
     @Step(name = "Submit Form", description = "Submit the login form")
